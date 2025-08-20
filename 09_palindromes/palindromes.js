6558c5b67132363ac601c3fe4883a828c6a4f7ab
lebuckman <liambuckman0@gmail.com>
2025-08-19 18:05:51 -0700

    Complete remaining exercises

--- 09_palindromes/palindromes.js
+++ 09_palindromes/palindromes.js
@@ -1,5 +1,15 @@
-const palindromes = function () {
+const palindromes = function (str) {
+    const VALID_CHARACTERS = "abcdefghijklmnopqrstuvwxyz1234567890";
 
+    const formattedStr = str
+        .toLowerCase()
+        .split("")
+        .filter((char) => VALID_CHARACTERS.includes(char))
+        .join("");
+
+    const reversedStr = formattedStr.split("").reverse().join("");
+
+    return formattedStr === reversedStr;
 };
 
 // Do not edit below this line
--- 10_fibonacci/fibonacci.js
+++ 10_fibonacci/fibonacci.js
@@ -1,5 +1,25 @@
-const fibonacci = function() {
+const fibonacci = function (n) {
+    let fib;
 
+    if (typeof n !== "number") {
+        fib = parseInt(n);
+    } else {
+        fib = n;
+    }
+
+    if (fib < 0) return "OOPS";
+    if (fib === 0) return 0;
+
+    let firstPrev = 1;
+    let secondPrev = 0;
+
+    for (let i = 2; i <= fib; i++) {
+        let current = firstPrev + secondPrev;
+        secondPrev = firstPrev;
+        firstPrev = current;
+    }
+
+    return firstPrev;
 };
 
 // Do not edit below this line
