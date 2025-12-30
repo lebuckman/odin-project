18e173c895780afca155be233a4298a48564ba5b
lebuckman <liambuckman0@gmail.com>
2025-12-30 13:42:48 -0800

    add uuid and book management
    - Added a unique id to Book constructor using crypto.randomUUID()
    - Added array that holds new Books
    - Added addBookToLibrary utility that calls the Book constructor

--- index.js
+++ index.js
@@ -1,9 +1,13 @@
+/********************
+ * BOOK CONSTRUCTOR *
+ ********************/
 function Book(title, author, pages, haveRead) {
     if (!new.target) {
         // Safeguard constructors
         throw Error("You must use the 'new' operator to call the constructor");
     }
 
+    this.id = crypto.randomUUID();
     this.title = title;
     this.author = author;
     this.pages = pages;
@@ -16,5 +20,22 @@ Book.prototype.info = function () {
     }`;
 };
 
-const theHobbit = new Book("The Hobbit", "J.R.R Tolkien", 295, false);
-console.log(theHobbit.info());
+/*************
+ * UTILITIES *
+ *************/
+
+const myLibrary = [];
+
+function addBookToLibrary(title, author, pages, haveRead) {
+    myLibrary.push(new Book(title, author, pages, haveRead));
+}
+
+/***********
+ * TESTING *
+ ***********/
+
+const theHobbit = addBookToLibrary("The Hobbit", "J.R.R Tolkien", 295, false);
+console.log(myLibrary);
+console.log(myLibrary[0].info());
+
+
